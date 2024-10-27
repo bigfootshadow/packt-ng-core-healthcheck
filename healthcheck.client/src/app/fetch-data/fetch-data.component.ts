9ce0a048b0a01a277d0aafc6a8aec982e67c9e05
Yurii Horiunov <y.gorunov@gmail.com>
2024-10-27 21:48:30 +0100

    updated components for navigation, home, and data fetch

--- healthcheck.client/src/app/fetch-data/fetch-data.component.ts
+++ healthcheck.client/src/app/fetch-data/fetch-data.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-fetch-data',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class FetchDataComponent {
 
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>(environment.baseUrl + 'api/WeatherForecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
