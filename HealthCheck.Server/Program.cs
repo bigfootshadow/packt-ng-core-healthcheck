ca1298763c3fa445e0d7beddac60c2a7f77d92e6
Yurii Horiunov <y.gorunov@gmail.com>
2024-10-30 06:36:06 +0100

    healhcheck added

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"));
 
 app.MapControllers();
 
